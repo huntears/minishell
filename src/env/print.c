7753bb6f9fd176512442c02c34ad7412a1cde69d
Alexandre Flion <huntears@kreog.com>
2024-02-14 23:52:35 +0100

    Fix env showing empty entries as (null)
    Fixes #1
    
    Signed-off-by: Alexandre Flion <huntears@kreog.com>

--- src/env/print.c
+++ src/env/print.c
@@ -4,6 +4,10 @@
 
 void print_env(const env_t *env)
 {
-    for (uint64_t i = 0; i < env->num_entry; i++)
-        printf("%s=%s\n", env->entries[i].key, env->entries[i].value);
+    for (uint64_t i = 0; i < env->num_entry; i++) {
+        if (env->entries[i].value)
+            printf("%s=%s\n", env->entries[i].key, env->entries[i].value);
+        else
+            printf("%s=\n", env->entries[i].key);
+    }
 }
